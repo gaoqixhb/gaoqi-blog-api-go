23c7573a08a35b719c8400dcd82721a980723d1c
luoyjx <yanjixiong@shimo.im>
2019-12-31 09:58:52 +0800

    chore: simple gin test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gaoqixhb/gaoqi-blog-api-go
 
 go 1.13
+
+require github.com/gin-gonic/gin v1.5.0
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main () {
-	fmt.Println("hello gaoqi blog api go!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "hello %s", name)
+	})
+
+	r.Run()
 }
